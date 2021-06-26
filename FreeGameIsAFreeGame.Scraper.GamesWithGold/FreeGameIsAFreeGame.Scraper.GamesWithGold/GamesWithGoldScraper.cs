b0d640eb5287293a750f5f66d7796f48a13c3536
Christiaan Bloemendaal <github@tnrd.net>
2021-06-26 20:29:01 +0200

    Updated dependencies

--- FreeGameIsAFreeGame.Scraper.GamesWithGold/FreeGameIsAFreeGame.Scraper.GamesWithGold/GamesWithGoldScraper.cs
+++ FreeGameIsAFreeGame.Scraper.GamesWithGold/FreeGameIsAFreeGame.Scraper.GamesWithGold/GamesWithGoldScraper.cs
@@ -20,7 +20,7 @@ namespace FreeGameIsAFreeGame.Scraper.GamesWithGold
         string IScraper.Identifier => "GamesWithGold";
 
         /// <inheritdoc />
-        public Task Initialize()
+        public Task Initialize(CancellationToken token)
         {
             context = BrowsingContext.New(Configuration.Default
                 .WithDefaultLoader()
